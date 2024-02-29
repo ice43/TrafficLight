1d87ac2166ea5e5ce8a14cc9a94c0794f9b6df90
bowski43 <43mngt@gmail.com>
2024-02-29 16:24:19 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Serge Broski on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
